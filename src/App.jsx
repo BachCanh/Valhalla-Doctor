3496b43aad0fb615187c326fa6db015323c21a7e
Nhat an <ngnhatan1205@gmail.com>
2025-06-18 11:23:12 +0700

    add appointment page for choosing a time slot

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import GeneralLayout from "./layouts/GeneralLayout";
 const HomePageGuest = lazy(() => import("./features/guest/Home/HomePage"));
 const Register = lazy(() => import("./features/guest/Register/Register"));
 const Login = lazy(() => import("./features/guest/Login/Login"));
-
+const Appointment = lazy(() => import("./features/guest/Appointment/AppointmentPage"))
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
@@ -16,6 +16,7 @@ function App() {
             <Route index element={<HomePageGuest />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/appointment" element={<Appointment />} />
           </Route>
         </Routes>
       </BrowserRouter>
